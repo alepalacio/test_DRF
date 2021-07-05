ca010cd03a05001894873788e1d4cb549a005a9f
alepalacio <alejandropalacio24@gmail.com>
2021-07-05 02:59:31 -0300

    Generic APIView added, api_view decorators removed

--- my_project/api_basic/urls.py
+++ my_project/api_basic/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
 """
-IMPORTING VIEWS FROM CLASS BASED VIEWS
+IMPORTING VIEWS FROM GENERICAPIVIEWS AND MIXINS
 """
-from .views import ArticleAPIView, SingleArticleAPIView
+from .views import GenericAPIView
+"""
+IMPORTING VIEWS FROM CLASS BASED VIEWS
+#from .views import ArticleAPIView, SingleArticleAPIView
+"""
 """
 IMPORTING VIEWS FROM FUNCTION BASED VIEWS.  NEED TO USE as_view()
-"""
 #from .views import article_list, article_single
+"""
 
 urlpatterns = [
-    
-    path('', ArticleAPIView.as_view()),
-    path('<int:pk>/', SingleArticleAPIView.as_view()),
+    path('generic/<int:id>/', GenericAPIView.as_view()),
+    #path('', ArticleAPIView.as_view()),
+    #path('<int:pk>/', SingleArticleAPIView.as_view()),
     #path('', article_list),
     #path('<int:pk>/', article_single),
 ]
--- my_project/api_basic/views.py
+++ my_project/api_basic/views.py
@@ -1,60 +1,120 @@
 from django.shortcuts import render
-#from django.http import HttpResponse, JsonResponse
-#from django.views.decorators.csrf import csrf_exempt
-#from rest_framework.decorators import api_view
+"""
+VIEWS IMPORTS
+from django.http import HttpResponse, JsonResponse
+"""
+
+"""
+@api_view DECORATORS IMPORTS
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+"""
 from rest_framework.response import Response
 from rest_framework import status
+
+"""
+APIView IMPORTS
 from rest_framework.views import APIView
+"""
+
+"""
+GENERIC VIEWS AND MIXINS IMPORTS
+"""
+from rest_framework import generics
+from rest_framework import mixins
+
+""""
+AUTHENTICATION
+"""
+#from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Article
 from .serializers import ArticleSerializer
 
 # Create your views here.
 """
-CLASS BASED VIEWS
+GENERIC VIEWS AND MIXINS
 """
-class ArticleAPIView(APIView):
-    def get(self, request):
-        if request.method == 'GET':
-            articles = Article.objects.all()
-            serializer = ArticleSerializer(articles, many=True)
-            return Response(serializer.data)
-        return Response('Cannot get full list')
+
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+
+    queryset = Article.objects.all()
+
+    lookup_field = 'id'
+
+    def get_serializer_class(self):
+        return ArticleSerializer
+
+    #Basic authentications
+    #authentication_classes = [SessionAuthentication, BasicAuthentication]
+
+    #Token authentication
+    authentication_classes = [TokenAuthentication]
+
+    permission_class = [IsAuthenticated]
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
 
     def post(self, request):
-        serializer = ArticleSerializer(data = request.data)
+        return self.create(request)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, id=None):
+        return self.update(request, id)
 
-class SingleArticleAPIView(APIView):
+    def delete(self, request, id):
+        return self.destroy(request, id)
+
+
+"""
+CLASS BASED VIEWS
+"""
+# class ArticleAPIView(APIView):
+#     def get(self, request):
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = ArticleSerializer(data = request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class SingleArticleAPIView(APIView):
     
-    #Getting data from a single element to work with it on differents views
+#     #Getting data from a single element to work with it on differents views
 
-    def get_article(self, pk):
-        try:
-            return Article.objects.get(id=pk)
-        except Article.DoesNotExist:
-            return Response(status=status.HTTP_404_BAD_REQUEST)
+#     def get_article(self, pk):
+#         try:
+#             return Article.objects.get(id=pk)
+#         except Article.DoesNotExist:
+#             return Response(status=status.HTTP_404_BAD_REQUEST)
 
-    def get(self, request, pk):
-        article = self.get_article(pk)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
+#     def get(self, request, pk):
+#         article = self.get_article(pk)
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
 
-    def put(self, request, pk):
-        article = self.get_article(pk)
-        serializer = ArticleSerializer(article, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         article = self.get_article(pk)
+#         serializer = ArticleSerializer(article, data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        article = self.get_article(pk)
-        article.delete()
-        return Response('Article successfully deleted')
+#     def delete(self, request, pk):
+#         article = self.get_article(pk)
+#         article.delete()
+#         return Response('Article successfully deleted')
 
 """
 @API_VIEW DECORATOR FOR FUNCTION BASED VIEWS
